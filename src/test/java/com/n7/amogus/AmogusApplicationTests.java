7a94fc43974ca71bd48982d6a18d3711f2ee441c
Bassou Mou <bassoumouacha@gmail.com>
2026-04-05 21:06:17 +0200

    first commit

---
+++ src/test/java/com/n7/amogus/AmogusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n7.amogus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmogusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
